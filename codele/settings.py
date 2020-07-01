5ef570b8c1e9b11984198c531dba2ec227a608b5
DOUGLAS EDUARDO REIS SILVA <douugbr@gmail.com>
2020-06-30 21:09:36 -0300

    Added 127.0.0.1 to allowed hosts

--- codele/settings.py
+++ codele/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', os.getenv('MY_IP'), '192.168.0.120', 'codele.digital']
+ALLOWED_HOSTS = ['127.0.0.1','localhost', os.getenv('MY_IP'), '192.168.0.120', 'codele.digital']
 
 # Application definition
 
